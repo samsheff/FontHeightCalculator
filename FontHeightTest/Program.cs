0e4f3c91fba749d87edb68bb7d11e48a86cf17cb
Sam Sheffres <samsheff@yandex.ru>
2020-03-29 15:24:19 -0700

    Initial Commit

---
+++ FontHeightTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FontHeightTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Graphics g = Graphics.FromImage( new Bitmap(1,1)))
+            {
+                var pixels = 11 * g.DpiX / 72;
+                MessageBox.Show(SystemFonts.DefaultFont.Name + " is at 72pt is " + pixels + " pixels per point");
+            }
+        }
+    }
+}
